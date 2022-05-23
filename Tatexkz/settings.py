658c04431db57f57684423535b83bd3b4d028b84
Nazar Kultayev <nazarcool2001@gmail.com>
2022-05-23 17:35:25 +0600

    Правки

--- Tatexkz/settings.py
+++ Tatexkz/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ SECRET_KEY = 'django-insecure-!amdrn@uz=4&9io$sz&lh867)&hmxk4v19u$(dx8^_@qrhtxva
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.6", "127.0.0.1", '192.168.1.2']
+ALLOWED_HOSTS = ["192.168.1.6", "127.0.0.1", '185.125.88.124']
 
 
 # Application definition
@@ -118,9 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR.joinpath('static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
