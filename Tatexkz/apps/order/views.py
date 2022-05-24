8958915dc141069ac6ca9fee22aaf598797dd49d
Nazar Kultayev <nazarcool2001@gmail.com>
2022-05-24 14:35:51 +0600

    rger

--- Tatexkz/apps/order/views.py
+++ Tatexkz/apps/order/views.py
@@ -77,7 +77,7 @@ def tariff(request):
                 DlvyDateTime = DlvyDateTime.text
             
             DlvyDateTime = datetime.strptime(DlvyDateTime, 
-                        '%Y-%m-%d %H:%M:%S').strftime("%d %m %Y")
+                        '%Y-%m-%d %H:%M:%S').strftime("%d.%m.%Y")
         if fromCountry == '' and whereCountry == '':
             for country in countries:
                 for city in countries[country]:
